e79e01499a9f86daa5c117851094305ae67b243d
Sanjay G M <gmsanjayganesh@gmail.com>
2026-05-25 11:13:25 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -0,0 +1,9 @@
+// Last updated: 5/25/2026, 11:13:24 AM
+class Solution {
+    public int divide(int d, int d1) {
+        if(d==-2147483648 && d1==-1)
+        return 2147483647;
+        return d/d1;
+        
+    }
+}
