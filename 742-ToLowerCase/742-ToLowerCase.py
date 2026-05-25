b7b3c32c69432a5781406a268c1f1aa8d5fb6d47
Sanjay G M <gmsanjayganesh@gmail.com>
2026-05-25 11:08:30 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.py
@@ -0,0 +1,9 @@
+# Last updated: 5/25/2026, 11:08:29 AM
+class Solution(object):
+    def toLowerCase(self, s):
+        if s.isupper():
+            return s.lower()
+        elif s.islower():
+            return s.lower()
+        else:
+            return s.lower()
