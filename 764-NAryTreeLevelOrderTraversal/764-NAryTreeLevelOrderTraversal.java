ab2db268cc20ecb8040555ff857301fa61b53933
Sanjay G M <gmsanjayganesh@gmail.com>
2026-05-25 11:08:25 +0530

    Create 764-NAryTreeLevelOrderTraversal

---
+++ 764-NAryTreeLevelOrderTraversal/764-NAryTreeLevelOrderTraversal.java
@@ -0,0 +1,48 @@
+// Last updated: 5/25/2026, 11:08:23 AM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                Node cur = q.poll();
+                level.add(cur.val);
+
+                for (Node j : cur.children) {
+                    q.offer(j);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
