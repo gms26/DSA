ab9e1f0e4e2b531d6984ceeae5f5fb5aedac7ff4
Sanjay G M <gmsanjayganesh@gmail.com>
2026-05-26 12:27:25 +0530

    File updated at 5/26/2026, 12:27:23 PM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,11 +1,11 @@
-// Last updated: 5/26/2026, 12:21:27 PM
+// Last updated: 5/26/2026, 12:27:23 PM
 1class Solution {
 2    public int maxProfit(int[] prices) {
 3        int sellone=0;
 4        int holdone=Integer.MIN_VALUE;
 5        int selltwo=0;
 6        int holdtwo=Integer.MIN_VALUE;
-7        for(final int price:prices){
+7        for(int price:prices){
 8            selltwo=Math.max(selltwo,holdtwo+price);
 9            holdtwo=Math.max(holdtwo,sellone-price);
 10            sellone=Math.max(sellone,holdone+price);
