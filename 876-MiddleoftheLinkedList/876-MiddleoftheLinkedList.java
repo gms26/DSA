5f3e047d59e063c16a5c66eee979808df0c9abb4
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-08 22:14:28 +0530

    File updated at 6/8/2026, 10:14:27 PM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 6/8/2026, 10:01:50 PM
+// Last updated: 6/8/2026, 10:14:27 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,32 +10,20 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-13        ListNode dummy = new ListNode(0);
-14        ListNode current = dummy;
-15
-16        
-17        while (list1 != null && list2 != null) {
-18            if (list1.val < list2.val) {
-19                current.next = list1;
-20                list1 = list1.next;
-21            } else {
-22                current.next = list2;
-23                list2 = list2.next;
-24            }
-25           
-26       
-27            current = current.next; 
-28        }
-29
-30      
-31        if (list1 != null) {
-32            current.next = list1;
-33        } else {
-34            current.next = list2;
-35        }
-36
-37       
-38        return dummy.next;
-39    }
-40}
+12    public ListNode removeNthFromEnd(ListNode head, int n) {
+13        ListNode cur=head;
+14        int i=0;
+15        
+16        while(cur!=null){
+17            i++;
+18            cur=cur.next;
+19        }
+20        if(i==n)return head.next;
+21        cur=head;
+22        for(int j=0;j<i-n-1;j++){
+23            cur=cur.next;
+24        }
+25        cur.next=cur.next.next;
+26        return head;
+27    }
+28}
