3f01809589c3722fe5b815ca92d5a93d5a484eca
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-10 00:03:30 +0530

    File updated at 6/10/2026, 12:03:29 AM

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,20 +1,25 @@
-// Last updated: 6/9/2026, 9:50:10 PM
-1class Solution {
-2    public int[] dailyTemperatures(int[] temperatures) {
-3        int n = temperatures.length;
-4        int[] res = new int[n];
-5        Stack<Integer> stack = new Stack<>(); 
-6
-7        for (int i = 0; i < n; i++) {
-8            
-9            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-10                int prevIndex = stack.pop();
-11                res[prevIndex] = i - prevIndex;
-12            }
-13            stack.push(i); 
-14        }
-15
-16        return res;
+// Last updated: 6/10/2026, 12:03:29 AM
+1class StockSpanner {
+2private Stack<int[]> stack;
+3
+4    public StockSpanner() {
+5        stack = new Stack<>();
+6    }
+7    
+8    public int next(int price) {
+9        int span = 1;
+10
+11        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+12            span += stack.pop()[1];
+13        }
+14
+15        stack.push(new int[]{price, span});
+16        return span;
 17    }
 18}
 19
+20/**
+21 * Your StockSpanner object will be instantiated and called as such:
+22 * StockSpanner obj = new StockSpanner();
+23 * int param_1 = obj.next(price);
+24 */
