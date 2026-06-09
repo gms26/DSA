3e9d7bf3db85805c1eaab1c721534db16c4686ac
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-09 21:50:11 +0530

    File updated at 6/9/2026, 9:50:10 PM

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,19 +1,20 @@
-// Last updated: 5/25/2026, 11:08:30 AM
-class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] res = new int[n];
-        Stack<Integer> stack = new Stack<>(); 
-
-        for (int i = 0; i < n; i++) {
-            
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int prevIndex = stack.pop();
-                res[prevIndex] = i - prevIndex;
-            }
-            stack.push(i); 
-        }
-
-        return res;
-    }
-}
+// Last updated: 6/9/2026, 9:50:10 PM
+1class Solution {
+2    public int[] dailyTemperatures(int[] temperatures) {
+3        int n = temperatures.length;
+4        int[] res = new int[n];
+5        Stack<Integer> stack = new Stack<>(); 
+6
+7        for (int i = 0; i < n; i++) {
+8            
+9            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+10                int prevIndex = stack.pop();
+11                res[prevIndex] = i - prevIndex;
+12            }
+13            stack.push(i); 
+14        }
+15
+16        return res;
+17    }
+18}
+19
