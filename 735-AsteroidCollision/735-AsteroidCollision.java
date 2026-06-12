49ecdf17d778c2a5cf1c2c7645abccbee541e2f6
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-12 22:36:18 +0530

    File updated at 6/12/2026, 10:36:17 PM

--- 735-AsteroidCollision/735-AsteroidCollision.java
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -1,38 +1,38 @@
-// Last updated: 5/25/2026, 11:08:32 AM
-import java.util.*;
-
-class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < asteroids.length; i++) {
-            int curr = asteroids[i];
-
-            while (!st.isEmpty() && st.peek() > 0 && curr < 0) {
-                int top = st.peek();
-
-                if (top == -curr) {
-                    st.pop();
-                    curr = 0;
-                    break;
-                } else if (top > -curr) {
-                    curr = 0;
-                    break;
-                } else {
-                    st.pop();
-                }
-            }
-
-            if (curr != 0) {
-                st.push(curr);
-            }
-        }
-
-        int[] z = new int[st.size()];
-        for (int i = st.size() - 1; i >= 0; i--) {
-            z[i] = st.pop();
-        }
-
-        return z;
-    }
-}
+// Last updated: 6/12/2026, 10:36:17 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int[] asteroidCollision(int[] asteroids) {
+5        Stack<Integer> st = new Stack<>();
+6
+7        for (int i = 0; i < asteroids.length; i++) {
+8            int curr = asteroids[i];
+9
+10            while (!st.isEmpty() && st.peek() > 0 && curr < 0) {
+11                int top = st.peek();
+12
+13                if (top == -curr) {
+14                    st.pop();
+15                    curr = 0;
+16                    break;
+17                } else if (top > -curr) {
+18                    curr = 0;
+19                    break;
+20                } else {
+21                    st.pop();
+22                }
+23            }
+24
+25            if (curr != 0) {
+26                st.push(curr);
+27            }
+28        }
+29
+30        int[] z = new int[st.size()];
+31        for (int i = st.size() - 1; i >= 0; i--) {
+32            z[i] = st.pop();
+33        }
+34
+35        return z;
+36    }
+37}
