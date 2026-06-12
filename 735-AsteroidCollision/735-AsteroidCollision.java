6b71ba8a5743551621dde47019cd2e0a2a6d4b78
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-12 23:33:09 +0530

    File updated at 6/12/2026, 11:33:08 PM

--- 735-AsteroidCollision/735-AsteroidCollision.java
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -1,38 +1,32 @@
-// Last updated: 6/12/2026, 10:36:17 PM
-1import java.util.*;
-2
-3class Solution {
-4    public int[] asteroidCollision(int[] asteroids) {
-5        Stack<Integer> st = new Stack<>();
-6
-7        for (int i = 0; i < asteroids.length; i++) {
-8            int curr = asteroids[i];
-9
-10            while (!st.isEmpty() && st.peek() > 0 && curr < 0) {
-11                int top = st.peek();
+// Last updated: 6/12/2026, 11:33:08 PM
+1class Solution {
+2    public String removeKdigits(String num, int k) {
+3        Stack<Character> st = new Stack<>();
+4
+5        for (char c : num.toCharArray()) {
+6            while (!st.isEmpty() && k > 0 && st.peek() > c) {
+7                st.pop();
+8                k--;
+9            }
+10            st.push(c);
+11        }
 12
-13                if (top == -curr) {
-14                    st.pop();
-15                    curr = 0;
-16                    break;
-17                } else if (top > -curr) {
-18                    curr = 0;
-19                    break;
-20                } else {
-21                    st.pop();
-22                }
-23            }
+13        while (k > 0) {
+14            st.pop();
+15            k--;
+16        }
+17
+18        StringBuilder sb = new StringBuilder();
+19        while (!st.isEmpty()) {
+20            sb.append(st.pop());
+21        }
+22
+23        sb.reverse();
 24
-25            if (curr != 0) {
-26                st.push(curr);
-27            }
-28        }
-29
-30        int[] z = new int[st.size()];
-31        for (int i = st.size() - 1; i >= 0; i--) {
-32            z[i] = st.pop();
-33        }
-34
-35        return z;
-36    }
-37}
+25        while (sb.length() > 1 && sb.charAt(0) == '0') {
+26            sb.deleteCharAt(0);
+27        }
+28
+29        return sb.length() == 0 ? "0" : sb.toString();
+30    }
+31}
