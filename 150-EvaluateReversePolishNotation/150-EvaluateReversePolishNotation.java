e1b35ef0f6918fdba4f1ec8dd249cacbdae89b68
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-16 10:04:04 +0530

    File updated at 6/16/2026, 10:04:03 AM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,30 +1,32 @@
-// Last updated: 6/14/2026, 10:22:02 PM
+// Last updated: 6/16/2026, 10:04:03 AM
 1class Solution {
 2    public int evalRPN(String[] tokens) {
-3        Stack<Integer> stack = new Stack<>();
-4
-5        for (String token : tokens) {
-6            if (token.equals("+")) {
-7                int b = stack.pop();
-8                int a = stack.pop();
-9                stack.push(a + b);
-10            } else if (token.equals("-")) {
-11                int b = stack.pop();
-12                int a = stack.pop();
-13                stack.push(a - b);
-14            } else if (token.equals("*")) {
-15                int b = stack.pop();
-16                int a = stack.pop();
-17                stack.push(a * b);
-18            } else if (token.equals("/")) {
-19                int b = stack.pop();
-20                int a = stack.pop();
-21                stack.push(a / b);
-22            } else {
-23                stack.push(Integer.parseInt(token));
-24            }
-25        }
-26
-27        return stack.pop();
-28    }
-29}
+3     Stack<Integer>st=new Stack<>();
+4     for(String s:tokens){
+5        if(s.equals("+")){
+6            int a=st.pop();
+7            int b=st.pop();
+8            st.push(b+a);
+9        }
+10        else if(s.equals("-")){
+11            int a=st.pop();
+12            int b=st.pop();
+13            st.push(b-a);
+14        }
+15        else if(s.equals("*")){
+16            int a=st.pop();
+17            int b=st.pop();
+18            st.push(b*a);
+19        }
+20        else if(s.equals("/")){
+21            int a=st.pop();
+22            int b=st.pop();
+23            st.push(b/a);
+24        }
+25        else{
+26            st.push(Integer.parseInt(s));
+27        }
+28     }
+29     return st.peek();
+30    }
+31}
