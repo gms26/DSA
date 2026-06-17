f39d0c624d4b8b9471ad10d3c1e2c0ca59c967cb
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-17 17:39:54 +0530

    File updated at 6/17/2026, 5:39:53 PM

--- 3781-MaximumScoreAfterBinarySwaps/3781-MaximumScoreAfterBinarySwaps.java
+++ 3781-MaximumScoreAfterBinarySwaps/3781-MaximumScoreAfterBinarySwaps.java
@@ -1,25 +1,11 @@
-// Last updated: 6/17/2026, 5:16:30 PM
+// Last updated: 6/17/2026, 5:39:53 PM
 1class Solution {
-2    public List<Integer> luckyNumbers(int[][] mat) {
-3        List<Integer>a=new ArrayList<>();
-4        boolean f=false;
-5        for(int i=0;i<mat.length;i++){
-6            int c=0;
-7            for(int j=0;j<mat[0].length;j++){
-8                if(mat[i][j]<mat[i][c]){
-9                    c=j;
-10                }
-11            }
-12            boolean s=false;
-13            int v=mat[i][c];
-14            for(int k=0;k<mat.length;k++){
-15                if(mat[k][c]>v){
-16                    s=true;
-17                    break;
-18                }
-19            }
-20            if(!s)a.add(v);
-21        }
-22        return a;
-23    }
-24}
+2    public int findKthLargest(int[] nums, int k) {
+3        PriorityQueue<Integer>pq=new PriorityQueue<>();
+4        for(int x:nums){
+5            pq.add(x);
+6            if(pq.size()>k)pq.poll();
+7        }
+8        return pq.peek();
+9    }
+10}
