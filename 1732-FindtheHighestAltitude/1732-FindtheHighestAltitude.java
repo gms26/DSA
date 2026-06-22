565f3047d94c0a136763af1471f209d02564de34
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-22 09:17:16 +0530

    File updated at 6/22/2026, 9:17:15 AM

--- 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.java
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.java
@@ -1,13 +1,15 @@
-// Last updated: 6/22/2026, 9:11:01 AM
+// Last updated: 6/22/2026, 9:17:15 AM
 1class Solution {
-2    public int largestAltitude(int[] gain) {
-3        int s=0;
-4        int m=0;
-5        for(int i=0;i<gain.length;i++){
-6            s+=gain[i];
-7            System.out.println(s);
-8            m=Math.max(m,s);
-9        }
-10        return m;
-11    }
-12}
+2    public int maxIceCream(int[] costs, int coins) {
+3        int c=0;
+4        int z=0;
+5        Arrays.sort(costs);
+6        for(int i=0;i<costs.length;i++){
+7            if(c+costs[i]<=coins){
+8                c+=costs[i];
+9                z++;
+10            }
+11        }
+12        return z;
+13    }
+14}
