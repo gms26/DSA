fe958641a68761cc05c339cd40b7e6b2ec0e47be
Sanjay G M <gmsanjayganesh@gmail.com>
2026-06-22 09:30:33 +0530

    File updated at 6/22/2026, 9:30:32 AM

--- 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.java
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.java
@@ -1,15 +1,19 @@
-// Last updated: 6/22/2026, 9:17:15 AM
+// Last updated: 6/22/2026, 9:30:32 AM
 1class Solution {
-2    public int maxIceCream(int[] costs, int coins) {
-3        int c=0;
-4        int z=0;
-5        Arrays.sort(costs);
-6        for(int i=0;i<costs.length;i++){
-7            if(c+costs[i]<=coins){
-8                c+=costs[i];
-9                z++;
-10            }
-11        }
-12        return z;
-13    }
-14}
+2    public int maxNumberOfBalloons(String text) {
+3        HashMap<Character, Integer> h = new HashMap<>();
+4
+5        for (char c : text.toCharArray()) {
+6            h.put(c, h.getOrDefault(c, 0) + 1);
+7        }
+8
+9        int b = h.getOrDefault('b', 0);
+10        int a = h.getOrDefault('a', 0);
+11        int l = h.getOrDefault('l', 0) / 2;
+12        int o = h.getOrDefault('o', 0) / 2;
+13        int n = h.getOrDefault('n', 0);
+14
+15        return Math.min(Math.min(b, a),
+16               Math.min(Math.min(l, o), n));
+17    }
+18}
