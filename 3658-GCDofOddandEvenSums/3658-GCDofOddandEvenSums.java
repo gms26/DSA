18ad7b34cf715bb4cd48437860c88c5a87b86d26
Sanjay G M <gmsanjayganesh@gmail.com>
2026-07-15 09:34:47 +0530

    File updated at 7/15/2026, 9:34:45 AM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
@@ -1,21 +1,25 @@
-// Last updated: 7/15/2026, 9:26:21 AM
+// Last updated: 7/15/2026, 9:34:45 AM
 1class Solution {
 2    int s=0;
 3    public int gcdOfOddEvenSums(int n) {
 4        int od=0,ev=0;
-5        for(int i=1;i<2*n;i++){
-6            if(i%2==0){
-7                ev+=i;
-8            }
-9            else{
-10                od+=i;
+5        // if(n==1){
+6        //     od=1;ev=0;}
+7        // else
+8        for(int i=1;i<=2*n;i++){
+9            if(i%2==0){
+10                ev+=i;
 11            }
-12        }
-13        s=gcd(od,ev);
-14        return s;
-15    }
-16    public int gcd(int a,int b){
-17        if(b<=1)return a;
-18        return gcd(b,a%b);
+12            else{
+13                od+=i;
+14            }
+15        }
+16       // System.out.print(od+" "+ev);
+17        s=gcd(od,ev);
+18        return s;
 19    }
-20}
+20    public int gcd(int a,int b){
+21        if(b==0)return a;
+22        return gcd(b,a%b);
+23    }
+24}
