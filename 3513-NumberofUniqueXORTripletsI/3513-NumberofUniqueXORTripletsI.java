9623935bededa5255b3f7d351bca5c187c4b59ad
Sanjay G M <gmsanjayganesh@gmail.com>
2026-07-25 10:17:19 +0530

    File updated at 7/25/2026, 10:17:18 AM

--- 3513-NumberofUniqueXORTripletsI/3513-NumberofUniqueXORTripletsI.java
+++ 3513-NumberofUniqueXORTripletsI/3513-NumberofUniqueXORTripletsI.java
@@ -1,21 +1,17 @@
-// Last updated: 7/25/2026, 10:10:26 AM
-class Solution {
-    public int maxProduct(int n) {
-        int max1 = 0, max2 = 0;
-
-        while (n > 0) {
-            int digit = n % 10;
-
-            if (digit > max1) {
-                max2 = max1;
-                max1 = digit;
-            } else if (digit > max2) {
-                max2 = digit;
-            }
-
-            n /= 10;
-        }
-
-        return max1 * max2;
-    }
-}
+// Last updated: 7/25/2026, 10:17:18 AM
+1class Solution {
+2    public int maxProduct(int n) {
+3        int z=0,c=0;
+4       // ArrayList<Integer>a=new ArrayList<>();
+5        while(n>0){
+6            int x=n%10;
+7           // a.add(x);
+8            if(z<x){c=z;z=x;}
+9            else if(c<x)c=x;
+10            n/=10;
+11        }
+12        return z*c;
+13        // Collections.sort(a);
+14        // return a.get(a.size()-1)*a.get(a.size()-2);
+15    }
+16}
