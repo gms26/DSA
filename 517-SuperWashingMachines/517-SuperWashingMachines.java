7c35deec6aa50d592c088eca8f5182f458c60c18
Sanjay G M <gmsanjayganesh@gmail.com>
2026-08-07 22:56:31 +0530

    Create 517-SuperWashingMachines

---
+++ 517-SuperWashingMachines/517-SuperWashingMachines.java
@@ -0,0 +1,19 @@
+// Last updated: 8/7/2026, 10:56:35 PM
+1class Solution {
+2    public int findMinMoves(int[] m) {
+3       
+4        int s=0;
+5        for(int i:m){
+6            s+=i;
+7        }
+8        int n=m.length;
+9        if(s%n!=0)return -1;
+10        int t=s/n,bal=0,ans=0;
+11        for(int i:m){
+12            int dif=i-t;
+13            bal+=dif;
+14            ans=Math.max(ans,Math.max(Math.abs(bal),dif));
+15        }return ans;
+16
+17    }
+18}
