8f46ae1a8213b4a28006c34674cdda12c8c89a6c
Sanjay G M <gmsanjayganesh@gmail.com>
2026-08-10 22:07:54 +0530

    File updated at 8/10/2026, 10:07:57 PM

--- 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.java
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.java
@@ -1,10 +1,16 @@
-// Last updated: 8/10/2026, 9:59:36 PM
+// Last updated: 8/10/2026, 10:07:57 PM
 1class Solution {
-2    public int[] buildArray(int[] nums) {
-3     int []ar=new int[nums.length];
-4     for(int i=0;i<nums.length;i++){
-5        ar[i]=nums[nums[i]];
-6     }   
-7     return ar;
-8    }
-9}
+2    public String interpret(String cmd) {
+3        String s="";
+4        for(int i=0;i<cmd.length();i++){
+5            char c=cmd.charAt(i);
+6            if(Character.isLetter(c)){
+7                s+=c+"";
+8            }
+9            else if(c=='(' && cmd.charAt(i+1)==')'){
+10                s+="o";
+11            }
+12        }
+13        return s;
+14    }
+15}
