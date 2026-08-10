c4fde5c65ae06983c3588e18c17e6e8067de0ae6
Sanjay G M <gmsanjayganesh@gmail.com>
2026-08-10 12:11:08 +0530

    Create 4353-LargestIntegerWithGivenDigitSum

---
+++ 4353-LargestIntegerWithGivenDigitSum/4353-LargestIntegerWithGivenDigitSum.java
@@ -0,0 +1,15 @@
+// Last updated: 8/10/2026, 12:11:12 PM
+class Solution {
+    public int largestInteger(int n, int s) {
+        if(s==0)return 0;
+     if(s>9*n)   
+     return -1;   
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n;i++){
+            int d=Math.min(9,s);
+            sb.append(d);
+            s-=d;
+        }
+        return Integer.parseInt(sb.toString());
+    }
+}
