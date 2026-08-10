b9735c12d9c2d90e34a36b4e363d45d6f4babc3a
Sanjay G M <gmsanjayganesh@gmail.com>
2026-08-10 12:17:55 +0530

    Create 1263-NumberOfDiceRollsWithTargetSum

---
+++ 1263-NumberOfDiceRollsWithTargetSum/1263-NumberOfDiceRollsWithTargetSum.java
@@ -0,0 +1,18 @@
+// Last updated: 8/10/2026, 12:17:58 PM
+class Solution {
+    public int numRollsToTarget(int n, int k, int target) {
+        int mod=1000000007;
+        int[][]dp=new int[n+1][target+1];
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                for(int m=1;m<=k;m++){
+                    if(j>=m){
+                        dp[i][j]=(dp[i][j]+dp[i-1][j-m])%mod;
+                    }
+                }
+            }
+        }
+        return dp[n][target];
+    }
+}
