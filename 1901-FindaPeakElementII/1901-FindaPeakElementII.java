d8840531fa604983f5b33b671e3e344ade757c1a
Sanjay G M <gmsanjayganesh@gmail.com>
2026-08-13 21:25:03 +0530

    File updated at 8/13/2026, 9:25:03 PM

--- 1901-FindaPeakElementII/1901-FindaPeakElementII.java
+++ 1901-FindaPeakElementII/1901-FindaPeakElementII.java
@@ -1,35 +1,24 @@
-// Last updated: 8/13/2026, 7:37:06 PM
+// Last updated: 8/13/2026, 9:25:03 PM
 1class Solution {
-2    public int[] findPeakGrid(int[][] mat) {
-3        //int[]ar=new int[2];
-4        int  m=mat.length,n=mat[0].length;
-5        for(int i=0;i<mat.length;i++){
+2    public int maximumPopulation(int[][] l) {
+3        int z=0;
+4        int m=0;
+5        for(int i=1950;i<=2050;i++){
 6            
-7            for(int j=0;j<mat[0].length;j++){
-8                boolean peak=true;
-9                if (i > 0 && mat[i][j] <= mat[i - 1][j]) {
-10                    peak = false;
-11                }
-12
-13            
-14                if (i < m - 1 && mat[i][j] <= mat[i + 1][j]) {
-15                    peak = false;
-16                }
-17
-18                
-19                if (j > 0 && mat[i][j] <= mat[i][j - 1]) {
-20                    peak = false;
-21                }
-22
-23                
-24                if (j < n - 1 && mat[i][j] <= mat[i][j + 1]) {
-25                    peak = false;
-26                }
-27if (peak) {
-28                    return new int[]{i, j};
-29                }
-30                
-31        }
-32        
-33    }return new int[]{-1,-1};
-34}}
+7            int c=0;
+8            for(int j=0;j<l.length;j++){
+9                
+10                    if(i>=l[j][0] && l[j][1]>i){
+11                        c++;
+12                    }
+13                
+14                
+15            }
+16            if(c>m){
+17                    m=c;
+18                    z=i;
+19                }
+20        }
+21        return z;
+22    }
+23}
